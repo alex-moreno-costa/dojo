084dd9191eedfd3df037f5c16d6e802493c2b461
Alex Moreno <alex.moreno.costa@gmail.com>
2018-11-16 21:58:19 -0200

    Consegui identificar os números de 1 a 9

--- src/OCR/OCR.php
+++ src/OCR/OCR.php
@@ -2,8 +2,32 @@
 
 namespace Dojo\OCR;
 
+use phpDocumentor\Reflection\Types\Integer;
 
 class OCR
 {
+    private $numberMap = [
+        1 => '     |  |',
+        2 => ' _  _||_ ',
+        3 => ' _  _| _|',
+        4 => '   |_|  |',
+        5 => ' _ |_  _|',
+        6 => ' _ |_ |_|',
+        7 => ' _   |  |',
+        8 => ' _ |_||_|',
+        9 => ' _ |_| _|'
+    ];
 
+    /**
+     * @param array $content
+     * @return int
+     */
+    public function identifyNumber(array $content): int
+    {
+        $string = implode('', $content);
+
+        if (false !== $number = array_search($string, $this->numberMap)) {
+            return $number;
+        }
+    }
 }
--- tests/OCR/OCRTest.php
+++ tests/OCR/OCRTest.php
@@ -2,9 +2,108 @@
 
 namespace Dojo\Tests\OCR;
 
+use Dojo\OCR\OCR;
 use PHPUnit\Framework\TestCase;
 
 class OCRTest extends TestCase
 {
+    /**
+     * @var OCR
+     */
+    private $ocr;
 
+    public function setUp()
+    {
+        $this->ocr = new OCR();
+    }
+
+    public function testIdentifyNumberOne()
+    {
+        $this->assertEquals(1, $this->ocr->identifyNumber([
+            "   ",
+            "  |",
+            "  |",
+            ""
+        ]));
+    }
+
+    public function testIdentifyNumberTwo()
+    {
+        $this->assertEquals(2, $this->ocr->identifyNumber([
+            " _ ",
+            " _|",
+            "|_ ",
+            ""
+        ]));
+    }
+
+    public function testIdentifyNumberThree()
+    {
+        $this->assertEquals(3, $this->ocr->identifyNumber([
+            " _ ",
+            " _|",
+            " _|",
+            ""
+        ]));
+    }
+
+    public function testIdentifyNumberFour()
+    {
+        $this->assertEquals(4, $this->ocr->identifyNumber([
+            "   ",
+            "|_|",
+            "  |",
+            ""
+        ]));
+    }
+
+    public function testIdentifyNumberFive()
+    {
+        $this->assertEquals(5, $this->ocr->identifyNumber([
+            " _ ",
+            "|_ ",
+            " _|",
+            ""
+        ]));
+    }
+
+    public function testIdentifyNumberSix()
+    {
+        $this->assertEquals(6, $this->ocr->identifyNumber([
+            " _ ",
+            "|_ ",
+            "|_|",
+            ""
+        ]));
+    }
+
+    public function testIdentifyNumberSeven()
+    {
+        $this->assertEquals(7, $this->ocr->identifyNumber([
+            " _ ",
+            "  |",
+            "  |",
+            ""
+        ]));
+    }
+
+    public function testIdentifyNumberEight()
+    {
+        $this->assertEquals(8, $this->ocr->identifyNumber([
+            " _ ",
+            "|_|",
+            "|_|",
+            ""
+        ]));
+    }
+
+    public function testIdentifyNumberNine()
+    {
+        $this->assertEquals(9, $this->ocr->identifyNumber([
+            " _ ",
+            "|_|",
+            " _|",
+            ""
+        ]));
+    }
 }
